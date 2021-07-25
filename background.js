31aae01492ec10310de6e921e398df6379acf88d
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-07-25 16:25:53 +0200

    init service worker script

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(function(request, message, sender, senderResponse){
+    if (request.message === 'buttonClicked'){
+        chrome.tabs.create({
+            active: true,
+            url: './addons/Snake/test_snake.html'
+        },null);
+    };
+});
