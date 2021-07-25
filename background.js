4c847507e919675961582c7cfe89667cf213fe05
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-07-25 17:03:00 +0200

    Major Update 2.0.0: - finally convert from manifest v.2 > v3

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(request, message, sender, senderRe
     if (request.message === 'buttonClicked'){
         chrome.tabs.create({
             active: true,
-            url: './addons/Snake/test_snake.html'
+            url: './addons/Snake/test_snake.html',   
         },null);
     };
 });
